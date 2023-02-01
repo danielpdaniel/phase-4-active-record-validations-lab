7b9798b5376672a3178d2afd7490ab1674627d58
danielpdaniel <artofjquinn@gmail.com>
2023-02-01 17:17:50 -0500

    commit tree 0e022185162f8266493a34eabb376a17a025fb90

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :clickbait_titles, 
+
+    def clickbait_titles
+        # byebug
+        if !title
+            errors.add(:title, "title cannot be blank!")
+        
+    elsif !title.downcase.include?("won't believe") && !title.downcase.include?("secret") && !title.downcase.include?("top ") && !title.downcase.include?("guess") 
+        errors.add(:title, "must include clickbait title!")
+       end
+    end
+
+
 end
